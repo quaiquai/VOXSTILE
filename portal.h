6e8b5c355d84800f3c0f7939c6f31f03296352b7
quaiquai <mmcquaig@uncc.edu>
2025-03-18 17:25:22 -0400

    fix race condition

--- portal.h
+++ portal.h
@@ -21,10 +21,10 @@ public:
 
 	float vertices[20] = {
 		// Positions          // Texture Coords
-		-0.5f, -0.5f, 0.0f,  0.0f, 0.0f, // Bottom-left
-		0.5f, -0.5f, 0.0f,  1.0f, 0.0f, // Bottom-right
-		0.5f,  0.5f, 0.0f,  1.0f, 1.0f, // Top-right
-		-0.5f,  0.5f, 0.0f,  0.0f, 1.0f  // Top-left
+		-1.0f, -1.0f, 0.0f,  0.0f, 0.0f, // Bottom-left
+		1.0f, -1.0f, 0.0f,  1.0f, 0.0f, // Bottom-right
+		1.0f,  1.0f, 0.0f,  1.0f, 1.0f, // Top-right
+		-1.0f,  1.0f, 0.0f,  0.0f, 1.0f  // Top-left
 	};
 
 	// Define the indices for the quad
